8bb964136c9d6801d3723efbe2702194e8968039
natal <natalka.swiercz@gmail.com>
2022-09-13 18:27:29 +0200

    initial commit

---
+++ src/main/java/com/finalproject/bugme/BugmeApplication.java
@@ -0,0 +1,13 @@
+package com.finalproject.bugme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugmeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BugmeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/finalproject/bugme/BugmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalproject.bugme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugmeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
