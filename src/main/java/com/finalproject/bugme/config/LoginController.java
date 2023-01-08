9d2758a70188a8cf507151103e290961a17b9bbf
natsw <natalka.swiercz@gmail.com>
2023-01-08 20:28:43 +0100

    spring security login page created

---
+++ src/main/java/com/finalproject/bugme/config/LoginController.java
@@ -0,0 +1,14 @@
+package com.finalproject.bugme.config;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping ("/login")
+    public String index(){
+        return "start/login";
+    }
+}
--- src/main/java/com/finalproject/bugme/config/SecurityConfig.java
+++ src/main/java/com/finalproject/bugme/config/SecurityConfig.java
@@ -1,4 +1,40 @@
 package com.finalproject.bugme.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests()
+                .requestMatchers("/css/**").permitAll()
+                .requestMatchers("/contact").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").permitAll();
+        return httpSecurity.build();
+
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(){
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        UserDetails user = User.withUsername("demo")
+                .password(encoder.encode("secret"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+
+    }
 }
