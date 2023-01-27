130b802ba37e20fb276e148e4da13b62acedfe29
natsw <natalka.swiercz@gmail.com>
2023-01-27 20:31:34 +0100

    Pagination for project list created

--- src/main/java/com/finalproject/bugme/project/ProjectController.java
+++ src/main/java/com/finalproject/bugme/project/ProjectController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.data.domain.Pageable;
-import java.util.List;
+
 
 @Controller
 @RequestMapping("/projects")
